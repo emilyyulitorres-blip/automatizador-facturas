addff1a25b5171d87a37528461bf1853b17677a9
emilyyulitorres-blip <emilyyulitorres@gmail.com>
2026-07-31 15:27:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent
 RUC_SOLARTEAM = "1793069479001"
 
 LOGO_CANDIDATES = [
-    BASE_DIR / "logo.png",
+    BASE_DIR / "logo_corregido.png",
 ]
 
 if os.name == "nt":
